b6903e9b7d77ef02077b406ccb73038f28d6a795
Alex Shearn <alex@isthat.it>
2012-06-20 16:50:38 +0100

    fixing sysupdate script.

--- script/sysupdate.sh
+++ script/sysupdate.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $DIR/../
-echo ">> Attempting `git pull` to update system. If it fails, navigate to the root of the app and attempt it manually."
+echo ">> Attempting <git pull> to update system. If it fails, navigate to the root of the app and attempt it manually."
 git pull origin master
 touch tmp/restart.txt
 cd -
